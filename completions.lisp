5c899ca4f3dca886894087c612b5b374ae64d545
Anthony Green <green@moxielogic.org>
2024-03-27 19:36:34 -0400

    Add basic support for completion functions (callbacks)

--- completions.lisp
+++ completions.lisp
@@ -33,26 +33,93 @@
    (api-key :initarg :api-key)
    (prompt-token-count :initform 0)
    (completion-token-count :initform 0)
-   (model :initarg :model :initform "gpt-4")))
+   (model :initarg :model :initform "gpt-4")
+   (tools :initarg :tools :initform (list))))
+
+(defclass tool ()
+  ())
+
+(defclass function-tool (tool)
+  ((description :initarg :description)
+   (name :initarg :name)
+   (parameters :initarg :parameters)
+   (fn :initarg :fn)))
+
+(defmethod render ((tool function-tool))
+  (with-slots (description name parameters) tool
+    `((:type . "function")
+      (:function . ((:name . ,name)
+                    (:description . ,description)
+                    ,(when parameters
+                       `(:parameters . ((:type . "object")
+                                        (:properties . ,(loop for p in parameters
+                                                              collect (cons (first p)
+                                                                            (list
+                                                                             (cons :type (second p))
+                                                                             (cons :description (third p))))))
+                                        (:required . ,(loop for p in parameters
+                                                            collect (first p)))))))))))
+
+(setf *tools* (make-hash-table :test 'equalp))
+
+(defmacro defun-tool (name args description &rest body)
+  ;; Compile time checks
+  (unless (listp args)
+    (error "ARGS must be a list."))
+  (unless (stringp description)
+    (error "DESCRIPTION must be a string."))
+  (dolist (arg args)
+    (let ((type (second arg)))
+      (unless (member (second arg) '(string number boolean) :test #'equal)
+        (error "Unsupported defun-tool argument type: ~a" (second arg)))))
+  ;; Generate code
+  (let ((name-str (if (symbolp name) (symbol-name name) name))
+        (arg-names (mapcar #'first args)))
+    `(progn
+       ;; Define and set the function in *tools*
+       (setf (gethash ,name-str *tools*)
+             (make-instance 'function-tool
+                            :name ,name-str
+                            :description ,description
+                            :parameters ',args
+                            :fn (lambda ,arg-names
+                                  ,@body))))))
+
+(defun completions-loop (provider endpoint headers payload)
+  (let ((response (drakma:http-request endpoint
+                                       :method :post
+                                       :content (cl-json:encode-json-to-string payload)
+                                       :additional-headers headers
+                                       :content-type "application/json")))
+    (let ((response (json:decode-json-from-string
+                     (flexi-streams:octets-to-string response :external-format :utf-8))))
+      (let ((first-choice (cdr (assoc :MESSAGE (cadr (assoc :CHOICES response))))))
+        (if (assoc :TOOL--CALLS first-choice)
+            (progn
+              (setf (cdr (assoc :MESSAGES payload))
+                    (append (cdr (assoc :MESSAGES payload)) (list first-choice)))
+              (setf (cdr (assoc :MESSAGES payload))
+                    (append (cdr (assoc :MESSAGES payload))
+                            (loop for tool-call in (cdr (assoc :TOOL--CALLS first-choice))
+                                  collect (let* ((fn-name (cdr (assoc :NAME (cdr (assoc :FUNCTION tool-call)))))
+                                                 (fn-tool (gethash fn-name *tools*)))
+                                            `((:role . "tool")
+                                              (:tool_call_id . ,(cdr (assoc :ID tool-call)))
+                                              (:content . ,(apply (slot-value fn-tool 'fn)
+                                                                    (let ((args (json:decode-json-from-string (cdr (assoc :ARGUMENTS (cdr (assoc :FUNCTION tool-call)))))))
+                                                                      (loop for arg in args
+                                                                            collect (cdr arg))))))))))
+              (completions-loop provider endpoint headers payload))
+            (cdr (assoc :CONTENT first-choice)))))))
 
 (defmethod get-completion ((provider openai-completions) starter-text max-tokens)
-  (with-slots (endpoint api-key model completion-token-count prompt-token-count) provider
-      (let* ((data `((:messages . (((:role . "user") (:content . ,starter-text))))
-                     (:model . ,model)
-                     (:max_tokens . ,max-tokens)))
-             (payload (cl-json:encode-json-to-string data))
-             (headers `(("Content-Type" . "application/json")
-                        ("Authorization" . ,(concatenate 'string "Bearer " api-key)))))
-        (let ((response (drakma:http-request endpoint
-                                             :method :post
-                                             :content payload
-                                             :additional-headers headers
-                                             :content-type "application/json")))
-          (let ((response (json:decode-json-from-string
-                           (flexi-streams:octets-to-string response :external-format :utf-8))))
-            (let ((usage (cdr (assoc :USAGE response))))
-              (incf completion-token-count (cdr (assoc :COMPLETION--TOKENS usage)))
-              (incf prompt-token-count (cdr (assoc :PROMPT--TOKENS usage))))
-            (cdr (assoc :CONTENT
-                        (cdr (assoc :MESSAGE
-                                    (car (cdr (assoc :CHOICES response))))))))))))
+  (with-slots (endpoint api-key model completion-token-count prompt-token-count tools) provider
+    (let* ((payload `((:messages . (((:role . "user") (:content . ,starter-text))))
+                      (:model . ,model)
+                      (:tools . ,(loop for key being the hash-keys of tools
+                                         using (hash-value value)
+                                       collect (render value)))
+                      (:max_tokens . ,max-tokens)))
+           (headers `(("Content-Type" . "application/json")
+                      ("Authorization" . ,(concatenate 'string "Bearer " api-key)))))
+      (completions-loop provider endpoint headers payload))))
